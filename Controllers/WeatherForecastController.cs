3749c7f3b322edeea0e321b22c0de6efe8101a4b
George Chakhidze <0xfeeddeadbeef@gmail.com>
2024-01-26 12:28:43 +0400

    Create the project

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet8DockerDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get(CancellationToken cancellation = default)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1d));
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace AspNet8DockerDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapControllers();
+        app.Run();
+    }
+}
